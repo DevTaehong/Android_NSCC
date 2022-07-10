4ed221ebacd1b36928c2f04728442fefbd3f4996
Taehong <minth1123@icloud.com>
2022-07-09 23:57:37 -0300

    Assignments

--- Quizprep/app/src/main/java/com/example/quizprep/BusClass.java
+++
@@ -1,7 +0,0 @@
-package com.example.quizprep;
-//business classes NEVER refer to UI
-public class BusClass {
-    private String busText = "Hello from Bus";
-
-    public String getBusText(){return this.busText;}
-}
